103ac1cdef7203ac1a0cea868216c7808acf2d66
Kevin Phan <kevin2006phan@gmail.com>
2025-04-27 03:30:12 +1000

    Update core.hpp

--- includes/fecs/core/core.hpp
+++ includes/fecs/core/core.hpp
@@ -12,14 +12,13 @@ namespace FECS
     template <typename T>
     using OwnedRef = std::unique_ptr<T>;
 
-    using EntityID = std::uint32_t;
     using SparseIndex = std::size_t;
 
     // constants
+    constexpr std::uint32_t INVALID_ENTITY_INDEX = std::numeric_limits<std::uint32_t>::max();
     constexpr std::size_t INVALID_SPARSE_INDEX = std::numeric_limits<SparseIndex>::max();
-    constexpr EntityID INVALID_ENTITY_ID = std::numeric_limits<EntityID>::max();
-    constexpr EntityID BASE_COMPONENT_AMOUNT = 64;
+    constexpr std::size_t BASE_COMPONENT_AMOUNT = 64;
 
     using ComponentSignature = std::bitset<BASE_COMPONENT_AMOUNT>;
 
-} 
+}
