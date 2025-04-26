7f598a2b62cea6313162b340e61105f760ad0d9f
Kevin Phan <kevin2006phan@gmail.com>
2025-04-27 06:06:15 +1000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,14 +2,39 @@
 #include <fecs/fecs.hpp>
 #include <fecs/core/entity/entity.hpp>
 
+struct Position
+{
+    float x;
+    float y;
+};
+
 int main()
 {
-    FECS::Registry registry;
+    try
+    {
+        FECS::Registry registry;
 
-    FECS::Entity e0 = registry.CreateEntity();
+        FECS::Entity e0 = registry.CreateEntity();
 
-    registry.AttachComponent<std::uint32_t>(e0, 32);
-    e0.AttachComponent<float>(64.0f);
+        registry.AttachComponent<std::uint32_t>(e0, 32);
+        e0.AttachComponent<float>(64.0f);
+
+        std::cout << e0.Get<float>() << std::endl;
+        std::cout << e0.Get<std::uint32_t>() << std::endl;
+
+        FECS::Entity e1 = registry.CreateEntity();
+        e1.AttachComponent<Position>({12.23f, 23.5f})
+            .AttachComponent<std::uint32_t>(12);
+
+        std::cout << e1.Get<Position>().x << " " << e1.Get<Position>().y << std::endl;
+        std::cout << e1.Get<std::uint32_t>() << std::endl;
+
+        e0.Get<Position>();
+    }
+    catch (std::exception e)
+    {
+        std::cout << e.what() << std::endl;
+    }
 
     return 0;
 }
