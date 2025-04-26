9f6e8f597899af0ac8a2423c898bf63299dec130
Kevin Phan <kevin2006phan@gmail.com>
2025-04-27 03:31:40 +1000

    Create entity.cpp

---
+++ src/core/entity/entity.cpp
@@ -0,0 +1,39 @@
+#include "fecs/core/core.hpp"
+#include <fecs/core/entity/entity.hpp>
+#include <fecs/manager/entity/entity_manager.hpp>
+
+namespace FECS
+{
+
+    Entity::Entity(Registry& registry, Manager::EntityManager& entityManager)
+        : m_RegistryReference(registry),
+          m_EntityManagerReference(entityManager)
+    {
+    }
+
+    std::uint32_t Entity::GetID() const
+    {
+        return m_ID;
+    }
+
+    void Entity::SetID(std::uint32_t id)
+    {
+        m_ID = id;
+    }
+
+    bool Entity::IsValid()
+    {
+        return m_ID != INVALID_ENTITY_INDEX;
+    }
+
+    void Entity::Destroy()
+    {
+        m_EntityManagerReference.DestroyEntity(*this);
+    }
+
+    // Operators
+    Entity::operator std::uint32_t() const
+    {
+        return GetID();
+    }
+}
