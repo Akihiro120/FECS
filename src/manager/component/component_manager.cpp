6145c138328ccf661c9cd950dd63f8b4c09d2805
Kevin Phan <kevin2006phan@gmail.com>
2025-04-27 23:34:58 +1000

    added entity removal

--- src/manager/component/component_manager.cpp
+++ src/manager/component/component_manager.cpp
@@ -6,8 +6,17 @@ namespace FECS
     namespace Manager
     {
 
-        ComponentManager::ComponentManager()
+        ComponentManager::ComponentManager(QueryManager& queryManager)
+            : m_QueryManagerReference(queryManager)
         {
+        }
+
+        void ComponentManager::RemoveEntity(std::uint32_t id)
+        {
+            for (auto& [key, storage] : m_ComponentPool)
+            {
+                storage->Remove(id);
+            }
         }
     }
 }
