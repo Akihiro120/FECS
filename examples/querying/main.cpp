a0bea5e780f0cf25a6179f26fb582fdfc57db26d
Kevin Phan <127700131+Akihiro120@users.noreply.github.com>
2025-11-23 22:28:22 +1100

    remake

--- examples/querying/main.cpp
+++ examples/querying/main.cpp
@@ -18,48 +18,4 @@ struct Health
 
 int main()
 {
-    FECS::Registry registry;
-    registry.RegisterComponent<Position>();
-    registry.RegisterComponent<Velocity>();
-    registry.RegisterComponent<Health>();
-
-    FECS::Entity e1 = registry.CreateEntity();
-    registry.Attach<Position>(e1, {1.0f, 2.0f});
-    registry.Attach<Velocity>(e1, {0.1f, 0.2f});
-    registry.Attach<Health>(e1, {100});
-
-    FECS::Entity e2 = registry.CreateEntity();
-    registry.Attach<Position>(e2, {3.0f, 4.0f});
-    registry.Attach<Velocity>(e2, {0.0f, 1.0f});
-
-    FECS::Entity e3 = registry.CreateEntity();
-    registry.Attach<Position>(e3, {5.0f, 6.0f});
-
-    // simple view, iterate Position + Velocity for all entities
-    std::cout << "All moving entities:" << std::endl;
-    registry.View<Position, Velocity>()
-        .Each([&](FECS::Entity e, Position& p, Velocity& v)
-    {
-        std::cout << "Entity " << e << " pos=(" << p.x << "," << p.y << ")"
-                  << " vel=(" << v.dx << "," << v.dy << ")" << std::endl;
-    });
-
-    // view with filter: only those with Health
-    std::cout << "Healthy Movers" << std::endl;
-    registry.View<Position, Velocity>()
-        .With<Health>()
-        .Each([&](FECS::Entity e, Position& p, Velocity& v)
-    {
-        int hp = registry.Get<Health>(e).hp;
-        std::cout << "Entity " << e << " has HP=" << hp << std::endl;
-    });
-
-    // view with exclusion: exclude those with Health
-    std::cout << "Movers without Health" << std::endl;
-    registry.View<Position, Velocity>()
-        .Without<Health>()
-        .Each([&](FECS::Entity e, Position& p, Velocity& v)
-    {
-        std::cout << "Entity " << e << " has no health." << std::endl;
-    });
 }
