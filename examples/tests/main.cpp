fc44f2b9242df7e1228e4999a2a31ce33b4efa70
Kevin Phan <127700131+Akihiro120@users.noreply.github.com>
2025-11-24 01:13:43 +1100

    worked on example

--- examples/tests/main.cpp
+++ examples/tests/main.cpp
@@ -1,9 +1,12 @@
 #include <fecs/fecs.h>
+#include <iostream>
 
 int main()
 {
     FECS::Entity id = FECS::Entities()->Create();
-    FECS::Components();
+    FECS::Components()->Attach<std::uint32_t>(id, 32);
+
+    std::cout << FECS::Components()->Get<std::uint32_t>(id) << std::endl;
 
     return 0;
 }
