4c6d83dcd4d869d72583e3554e68c0fa82bb272b
Kevin Phan <kevin2006phan@gmail.com>
2025-11-25 03:34:09 +1100

    reworking example

--- examples/tests/main.cpp
+++ examples/tests/main.cpp
@@ -1,56 +1,23 @@
-#include <iostream>
 #include <FECS/FECS.h>
 
 struct Position
 {
-    float x = 0.0f;
-    float y = 0.0f;
+    float x;
+    float y;
 };
 
-std::ostream& operator<<(std::ostream& stream, Position& pos)
+struct Velocity
 {
-    return stream << "Position: {" << pos.x << ", " << pos.y << "}";
-}
-
-namespace Prefabs
-{
-    auto PlayerPrefab(FECS::Builder::EntityBuilder& builder) -> void
-    {
-        builder
-            .Attach<Position>(Position{32.0f, 18.0f});
-    }
-}
-
-auto CreatePlayer(FECS::World& world) -> FECS::Entity
-{
-    FECS::Entity e = world.Entities()
-                         .Create()
-                         .Apply(Prefabs::PlayerPrefab)
-                         .Build();
-
-    return e;
-}
-
-auto ModifyPlayer(FECS::World& world, FECS::Entity entity) -> void
-{
-    world.Entities()
-        .Modify(entity)
-        .Patch<Position>([](Position& position)
-    {
-        position.x = 128;
-        position.y = 256;
-    });
-}
+    float x;
+    float y;
+};
 
 auto main() -> int
 {
-    FECS::World world = FECS::Init();
-    FECS::Entity e = CreatePlayer(world);
-
-    std::cout << world.Components().Get<Position>(e) << std::endl;
-
-    ModifyPlayer(world, e);
-    std::cout << world.Components().Get<Position>(e) << std::endl;
+    FECS::World world;
+    world.View()
+        .Query<Position, Velocity>()
+        .Each([](FECS::Entity id, Position& pos, Velocity& vel) {});
 
     return 0;
 }
