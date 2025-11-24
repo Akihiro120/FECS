64de7b38f2e4143aa37e8ba15041e32a0e4293d2
Kevin Phan <kevin2006phan@gmail.com>
2025-11-25 03:34:29 +1100

    adding view to world

--- src/FECS/World.h
+++ src/FECS/World.h
@@ -4,6 +4,7 @@
 #include "FECS/Manager/ComponentManager.h"
 #include "FECS/Manager/ResourceManager.h"
 #include "FECS/Manager/ScheduleManager.h"
+#include "FECS/Manager/ViewManager.h"
 
 namespace FECS
 {
@@ -18,6 +19,7 @@ namespace FECS
             p_EntityManager = std::make_unique<EntityManager>(p_ComponentManager);
             p_ResourceManager = std::make_unique<ResourceManager>();
             p_ScheduleManager = std::make_unique<ScheduleManager>();
+            p_ViewManager = std::make_unique<ViewManager>(p_ComponentManager);
         }
 
         ~World() = default;
@@ -32,11 +34,17 @@ namespace FECS
             return *p_ComponentManager;
         }
 
+        auto View() -> ViewManager&
+        {
+            return *p_ViewManager;
+        }
+
     private:
         // managers
         std::unique_ptr<EntityManager> p_EntityManager;
         std::unique_ptr<ComponentManager> p_ComponentManager;
         std::unique_ptr<ResourceManager> p_ResourceManager;
         std::unique_ptr<ScheduleManager> p_ScheduleManager;
+        std::unique_ptr<ViewManager> p_ViewManager;
     };
 }
