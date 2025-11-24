bba5c79bcb2ba5001342a5ca05c82e427ee81ffc
Kevin Phan <kevin2006phan@gmail.com>
2025-11-24 18:42:03 +1100

    built on the entity builder

--- src/FECS/Builder/EntityBuilder.h
+++ src/FECS/Builder/EntityBuilder.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "FECS/Core/Types.h"
 #include "FECS/Manager/ComponentManager.h"
+#include <functional>
 #include <memory>
 
 namespace FECS::Builder
@@ -10,26 +11,53 @@ namespace FECS::Builder
     class EntityBuilder
     {
     public:
-        EntityBuilder(Entity entity, std::shared_ptr<ComponentManager> manager)
+        EntityBuilder(Entity entity, std::unique_ptr<ComponentManager>& manager)
             : p_ComponentManager(manager)
         {
             m_ModifiedEntity = entity;
         }
 
-        auto Build() -> Entity;
-
-        template <typename T>
-        auto Attach(const T& component) -> EntityBuilder&
+        inline auto Build() -> Entity
         {
-            p_ComponentManager->Attach<T>(m_ModifiedEntity, component);
+            return m_ModifiedEntity;
         }
 
-        auto Detach() -> EntityBuilder&
+        template <typename T>
+        inline auto Attach(const T& component) -> EntityBuilder&
         {
+            p_ComponentManager->Attach<T>(m_ModifiedEntity, component);
+            return *this;
+        }
+
+        template <typename T>
+        inline auto Attach(T&& component) -> EntityBuilder&
+        {
+            p_ComponentManager->Attach<T>(m_ModifiedEntity, std::move(component));
+            return *this;
+        }
+
+        template <typename T, typename... Args>
+        inline auto Emplace(Args&&... args) -> EntityBuilder&
+        {
+            p_ComponentManager->Emplace<T>(m_ModifiedEntity, std::forward<Args>(args)...);
+            return *this;
+        }
+
+        template <typename T>
+        inline auto Tag(const T& component) -> EntityBuilder&
+        {
+            p_ComponentManager->Attach<T>(m_ModifiedEntity, T{});
+            return *this;
+        }
+
+        inline auto Apply(std::function<void(EntityBuilder&)> func) -> EntityBuilder&
+        {
+            func(*this);
+            return *this;
         }
 
     private:
         Entity m_ModifiedEntity = INVALID_ENTITY;
-        std::shared_ptr<ComponentManager> p_ComponentManager;
+        std::unique_ptr<ComponentManager>& p_ComponentManager;
     };
 }
