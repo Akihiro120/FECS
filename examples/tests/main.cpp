cd8f03622e88fd9ee48dc40e891e020e73ed9cd9
Kevin Phan <127700131+Akihiro120@users.noreply.github.com>
2025-11-25 23:10:10 +1100

    worked on example

--- examples/tests/main.cpp
+++ examples/tests/main.cpp
@@ -12,9 +12,32 @@ struct Velocity
     float y;
 };
 
+#define MAX_ENTITIES 123
+auto ConstructEntities(FECS::World& world) -> void
+{
+    for (int i = 0; i < MAX_ENTITIES; i++)
+    {
+        world.Entities()
+            .Create()
+            .Attach(Position{(float) i, (float) i})
+            .When(i % 2 == 0, [&i](FECS::Builder::EntityBuilder& builder)
+        {
+            builder
+                .Attach(Velocity{(float) i * 32.0f, (float) i * 32.0f})
+                .Patch<Position>([&i](Position& pos)
+            {
+                pos.x = (float) i * 16.0f;
+                pos.y = (float) i * 16.0f;
+            });
+        }).Build();
+    }
+}
+
 auto main() -> int
 {
     FECS::World world;
+    ConstructEntities(world);
+
     world.View()
         .Query<Position, Velocity>()
         .Each([](FECS::Entity id, Position& pos, Velocity& vel) {});
