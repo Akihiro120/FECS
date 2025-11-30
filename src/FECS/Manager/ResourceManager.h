5d5d56dffb852b42ffa40b48b69a3deb95486bed
Kevin Phan <127700131+Akihiro120@users.noreply.github.com>
2025-12-01 01:17:56 +1100

    working on system schduling

--- src/FECS/Manager/ResourceManager.h
+++ src/FECS/Manager/ResourceManager.h
@@ -1,4 +1,8 @@
 #pragma once
+#include <vector>
+#include <unordered_map>
+#include <typeindex>
+#include <any>
 
 namespace FECS::Manager
 {
@@ -8,5 +12,57 @@ namespace FECS::Manager
         ResourceManager()
         {
         }
+
+        template <typename T>
+        auto Add(const T& resource) -> void
+        {
+            auto typeId = std::type_index(typeid(T));
+            auto it = m_Lookup.find(typeId);
+
+            if (it != m_Lookup.end())
+            {
+                m_Data[it->second] = resource;
+                return;
+            }
+
+            m_Lookup[typeId] = m_Data.size();
+            m_Data.emplace_back(resource);
+        }
+
+        template <typename T, typename... Args>
+        auto Emplace(Args&&... args) -> void
+        {
+            auto typeId = std::type_index(typeid(T));
+            auto it = m_Lookup.find(typeId);
+            if (it != m_Lookup.end())
+            {
+                m_Data[it->second] = std::make_any<T>(std::forward<Args>(args)...);
+                return;
+            }
+
+            m_Lookup[typeId] = m_Data.size();
+            m_Data.emplace_back(std::make_any<T>(std::forward<Args>(args)...));
+        }
+
+        template <typename T>
+        auto Has() const -> bool
+        {
+            return m_Lookup.find(std::type_index(typeid(T))) != m_Lookup.end();
+        }
+
+        template <typename T>
+        auto Get() -> T&
+        {
+            auto typeId = std::type_index(typeid(T));
+            auto it = m_Lookup.find(typeId);
+
+            assert(it != m_Lookup.end() && "ResourceManager - Resource does not exist.");
+
+            return std::any_cast<T&>(m_Data[it->second]);
+        }
+
+    private:
+        std::vector<std::any> m_Data;
+        std::unordered_map<std::type_index, size_t> m_Lookup;
     };
 }
--- src/FECS/Manager/ScheduleManager.h
+++ src/FECS/Manager/ScheduleManager.h
@@ -1,12 +1,81 @@
 #pragma once
+#include <functional>
+#include <vector>
+#include <string>
+#include "FECS/Builder/SystemBuilder.h"
+#include "FECS/World.h"
 
 namespace FECS::Manager
 {
+    using namespace Builder;
+
+    enum class SystemMode
+    {
+        UPDATE,
+        FIXED,
+        TIMED
+    };
+
+    struct TimedSystem
+    {
+        std::function<void(World&)> func;
+        float interval;
+        float accumulator = 0.0f;
+    };
+
+    struct SystemSet
+    {
+        std::vector<std::function<void(World&)>> updateSystem;
+        std::vector<std::function<void(World&)>> fixedSystem;
+        std::vector<TimedSystem> timedSystems;
+    };
+
     class ScheduleManager
     {
+
     public:
-        ScheduleManager()
+        ScheduleManager(World& world)
+            : m_World(world)
+        {
+            m_SetExecutionOrder.push_back("Default");
+        }
+
+        auto SetExecutionOrder(const std::vector<std::string>& order) -> void
+        {
+            m_SetExecutionOrder = order;
+        }
+
+        auto AddSystem() -> SystemBuilder<>
+        {
+            return SystemBuilder<>(m_World, *this);
+        }
+
+        auto Run() -> void
         {
         }
+
+    private:
+        auto RegisterSystem(std::function<void(World&)> func) -> void
+        {
+        }
+
+    private:
+        friend SystemBuilder<>;
+
+        World& m_World;
+        float m_FixedStep;
+        float m_GlobalFixedAccumulator = 0.0f;
+
+        std::unordered_map<std::string, SystemSet> m_Sets;
+        std::vector<std::string> m_SetExecutionOrder;
     };
+}
+
+namespace FECS::Builder
+{
+    template <typename... Args>
+    auto SystemBuilder<Args...>::RegisterToScheduler(std::function<void(World&)> func) -> void
+    {
+        m_ScheduleManager.RegisterSystem(std::move(func));
+    }
 }
