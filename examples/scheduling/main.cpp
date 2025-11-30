52521acccaa06c3ba37f0049e95273135924f164
Kevin Phan <127700131+Akihiro120@users.noreply.github.com>
2025-12-01 01:17:13 +1100

    work

---
+++ examples/scheduling/main.cpp
@@ -0,0 +1,73 @@
+#include <FECS/FECS.h>
+#include <iostream>
+
+using namespace FECS;
+
+struct Position
+{
+    float x;
+    float y;
+};
+
+std::ostream& operator<<(std::ostream& stream, Position& pos)
+{
+    return stream << "Position: x:" << pos.x << ", y:" << pos.y;
+}
+
+struct Velocity
+{
+    float x;
+    float y;
+};
+
+std::ostream& operator<<(std::ostream& stream, Velocity& vel)
+{
+    return stream << "Velocity: x:" << vel.x << ", y:" << vel.y;
+}
+
+struct Time
+{
+    float time;
+
+    auto deltaTime() const -> float
+    {
+        return time;
+    }
+};
+
+auto MovementSystem(const Time& time, QueryBuilder<Position, Velocity> query) -> void
+{
+    query.Each([&](Entity id, Position& pos, Velocity& vel)
+    {
+        std::cout << pos << std::endl;
+        pos.x += vel.x * time.deltaTime();
+        pos.y += vel.y * time.deltaTime();
+    });
+
+    query.Each([&](Entity id, Position& pos, Velocity& vel)
+    {
+        std::cout << pos << std::endl;
+    });
+}
+
+auto main() -> int
+{
+    World world;
+    world.Entities()
+        .Create()
+        .Attach<Position>({6.0f, 6.0f})
+        .Attach<Velocity>({12.0f, 12.0f})
+        .Build();
+
+    world.Resources().Add<Time>({32.0f});
+
+    world.Scheduler()
+        .AddSystem()
+        .Read<Time>()
+        .WithQuery<Position, Velocity>()
+        .Build(MovementSystem);
+
+    world.Scheduler().Run();
+
+    return 0;
+}
