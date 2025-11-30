b60b1daae37026cf929707a235c61992f8ce0c2f
Kevin Phan <127700131+Akihiro120@users.noreply.github.com>
2025-12-01 01:18:03 +1100

    added cmpleted types

--- src/FECS/FECS.h
+++ src/FECS/FECS.h
@@ -1,5 +1,10 @@
 #pragma once
 #include "FECS/World.h"
+#include "FECS/Manager/ScheduleManager.h"
+#include "FECS/Manager/ComponentManager.h"
+#include "FECS/Manager/EntityManager.h"
+#include "FECS/Manager/ResourceManager.h"
+#include "FECS/Manager/ViewManager.h"
 
 namespace FECS
 {
