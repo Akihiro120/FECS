50adf4e38b39f210fe88d529e5cd15f3325903ba
Kevin Phan <kevin2006phan@gmail.com>
2026-03-19 23:20:59 +1100

    added testing suite

---
+++ examples/testing/main.cpp
@@ -0,0 +1,25 @@
+#include "iostream"
+#include <FECS/World/World.h>
+
+struct Player
+{
+
+};
+
+struct Position 
+{
+    float x;
+    float y;
+};
+
+struct Velocity
+{
+    float dx;
+    float dy;
+};
+
+int main()
+{
+    FECS::World world;
+    return 0;
+}
