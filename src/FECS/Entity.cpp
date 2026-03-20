8ac041795cddc91441486f373db4186dc2d8a614
Kevin Phan <kevin2006phan@gmail.com>
2026-03-20 20:22:21 +1100

    added index and versioning

--- src/FECS/Entity.cpp
+++ src/FECS/Entity.cpp
@@ -1,14 +1,35 @@
 #include "Entity.h"
+// #include <iostream>
 
 namespace FECS
 {
+    static constexpr std::uint32_t INDEX_BITS = 20;
+    // static constexpr std::uint32_t VERSION_BITS = 12;
+    static constexpr std::uint32_t INDEX_MASK = (1u << INDEX_BITS) - 1;
+    static constexpr std::uint32_t VERSION_MASK = ~INDEX_MASK;
+
     Entity::Entity(std::uint32_t idx, std::uint32_t ver)
     {
+        this->m_Index = (ver << INDEX_BITS) | (idx & INDEX_MASK);
+    }
 
+    Entity::Entity(std::uint32_t idx)
+    {
+        this->m_Index = idx;
     }
 
     Entity::~Entity()
     {
+        // std::cout << "Deconstructed Entity Index: " << GetIndex() << " Generation: " << GetVersion() << std::endl;
+    }
 
+    std::uint32_t Entity::GetIndex()
+    {
+        return m_Index & INDEX_MASK;
+    }
+
+    std::uint32_t Entity::GetVersion()
+    {
+        return (m_Index & VERSION_MASK) >> INDEX_BITS;
     }
 }
--- src/FECS/Entity.h
+++ src/FECS/Entity.h
@@ -7,7 +7,11 @@ namespace FECS
     {
     public:
         Entity(std::uint32_t idx, std::uint32_t ver);
+        Entity(std::uint32_t idx);
         ~Entity();
+
+        std::uint32_t GetIndex();
+        std::uint32_t GetVersion();
 
     private:
         std::uint32_t m_Index = 0;
