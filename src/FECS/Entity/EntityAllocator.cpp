f50855a59ef734547b8a0d901649add2d92f682d
Kevin Phan <kevin2006phan@gmail.com>
2026-03-20 20:22:28 +1100

    started entity allocation

---
+++ src/FECS/Entity/EntityAllocator.cpp
@@ -0,0 +1,15 @@
+#pragma once
+#include "EntityAllocator.h"
+
+namespace FECS
+{
+    EntityAllocator::EntityAllocator()
+    {
+
+    }
+
+    EntityAllocator::~EntityAllocator()
+    {
+
+    }
+}
---
+++ src/FECS/Entity/EntityAllocator.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace FECS
+{
+    class EntityAllocator
+    {
+    public:
+        EntityAllocator();
+        ~EntityAllocator();
+    };
+}
